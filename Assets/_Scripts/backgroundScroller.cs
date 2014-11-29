2ba4b9bb7291cdf55a5af2afebf33f61679dcf71
edbles <edbles@gmail.com>
2014-11-29 16:25:16 -0500

    Scrolling Background
    Added a Scrolling background
    Made stair prefabs
    Set camera

---
+++ Assets/_Scripts/backgroundScroller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundScroller : MonoBehaviour {
+
+	public float scrollSpeed;
+
+	private Vector2 savedOffset;
+
+
+	// Use this for initialization
+	void Start () {
+
+		savedOffset = renderer.sharedMaterial.GetTextureOffset ("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = Mathf.Repeat (Time.time*scrollSpeed, 6);
+		Vector2 scrollOffset = new Vector2(x, savedOffset.y);
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", scrollOffset); 
+
+	
+	}
+
+	void OnDisable (){
+	
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+
+
+	}
+}
