83777cb337c6cbecaf90439dcac80bb8c55f7bd3
edbles <edbles@gmail.com>
2014-12-01 21:06:34 -0500

    Player Movement Camera Fix
    -added player controls
    -added level boundaries
    -made camera child of player and reshaped boundary

---
+++ Assets/_Scripts/playerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float horizontalSpeed;
+	public float verticalSpeed;
+
+	private Vector2 playerForce;
+	private Vector2 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+		playerForce = new Vector2 (0, 0);
+		startingPosition = rigidbody2D.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = Vector2.zero;
+		playerForce.Set (0.0f, 0.0f);
+		if(Input.GetKey(KeyCode.UpArrow)||Input.GetKey (KeyCode.W)){
+			playerForce.y += verticalSpeed;
+		}
+		if(Input.GetKey(KeyCode.DownArrow)||Input.GetKey (KeyCode.S)){
+			playerForce.y -= verticalSpeed;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey (KeyCode.A)){
+			playerForce.x -= horizontalSpeed;
+		}
+		if(Input.GetKey(KeyCode.RightArrow)||Input.GetKey (KeyCode.D)){
+			playerForce.x += horizontalSpeed;
+		}
+		rigidbody2D.AddForce (playerForce);
+
+
+	
+	}
+
+	void OnDisable(){
+		rigidbody2D.position = startingPosition;
+	}
+}
