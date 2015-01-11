f88f00f9ad7569fb5aa5dd20f481f61f77788029
edbles <edbles@gmail.com>
2015-01-10 22:21:50 -0500

    Stair Fixes
    Got Stairs to delete when they hit level boundary

---
+++ Assets/_Scripts/DestroyByBoundary.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+
+
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.tag == "upRamp" || other.tag == "downRamp") {
+
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Assets/objectMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		rigidbody2D.velocity = new Vector2(-2.0f, 0);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
